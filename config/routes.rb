4cda27f21db9498510e1cb8f8439170c28be5006
DamiC0de <dreymidot@gmail.com>
2023-06-07 12:24:40 +0200

    Logged-in users can now add photos to their basket, view their basket and delete photos from their basket.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   post '/add_to_cart/:product_id', to: 'carts#add_to_cart', as: 'add_to_cart'
   delete '/remove_from_cart/:product_id', to: 'carts#remove_from_cart', as: 'remove_from_cart'
 
+  get 'users/profile'
+
   # d'autres routes ici...
 end
