00e0727c8d1cd31e06123c7d0030958ab8348695
alexis <dupray_a@hotmail.fr>
2023-06-07 11:59:49 +0200

    mailers

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,31 @@
+class UserMailer < ApplicationMailer
+  default :from => 'kitten.shop@gmail.com'
+
+    def welcome_email(user)
+        #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+        @user = user 
+    
+        #on définit une variable @url qu'on utilisera dans la view d’e-mail
+        @url  = 'www.kitten_shop.com' 
+    
+        # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+        mail(to: @user.email, subject: 'Bienvenue chez nous !') 
+ 
+
+      end
+
+
+      def confirmation_email(user,order)
+        #on récupère les instances user & order pour ensuite pouvoir les passer à la view en @user
+        @user = user 
+        @order = order
+    
+        #on définit une variable @url qu'on utilisera dans la view d’e-mail
+        @url  = 'http://kitten_shop/login' 
+    
+        # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+        mail(to: @user.email, subject: "Merci pour votre achat N° #{@order.id} !") 
+ 
+
+      end
+end
