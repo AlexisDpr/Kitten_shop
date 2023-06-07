29c40e50d58ebccd90022494d9d08212900bc9fc
DamiC0de <dreymidot@gmail.com>
2023-06-07 15:29:57 +0200

    change name of routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   post '/add_to_cart/:product_id', to: 'carts#add_to_cart', as: 'add_to_cart'
   post '/test_order/', to: 'carts#test_order', as: 'test_order'
   delete '/remove_from_cart/:product_id', to: 'carts#remove_from_cart', as: 'remove_from_cart'
-  get 'users/profile', to: 'users#profile', as: 'user_profile'
 
-
+  get 'myprofile', to: 'users#profile', as: 'user_profile'
+  get 'mycart', to: 'carts#show', as: 'mycart'
   # d'autres routes ici...
 end
