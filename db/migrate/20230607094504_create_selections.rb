6cebafb2d58d96d92d19dba203b15d173ba43ea4
DamiC0de <dreymidot@gmail.com>
2023-06-07 11:54:59 +0200

    add model selection

---
+++ db/migrate/20230607094504_create_selections.rb
@@ -0,0 +1,10 @@
+class CreateSelections < ActiveRecord::Migration[7.0]
+  def change
+    create_table :selections do |t|
+      t.references :cart, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
