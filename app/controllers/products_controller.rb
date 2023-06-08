b374341bfecd375808f26849de06a00f788fc697
Valentin Da silva <v.dasilva11d@gmail.com>
2023-06-08 15:36:55 +0200

    Admin works

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,32 @@ class ProductsController < ApplicationController
     def show
       @product = Product.find(params[:id])
     end
-  
+
+    def edit
+      admin_check
+      @product = Product.find(params[:id])
+    end
+
+    def update
+      admin_check
+      @product = Product.find(params[:id])
+      if @product.update(name: params["name"],price: params["price"],description:params["description"],image: params["url"])
+          redirect_to "/products/#{@product.id}"
+      else
+          redirect_to "/Admin/edit"
+      end
+  end
+
+  def admin_check
+    if current_user
+        if current_user.is_admin
+        else
+            redirect_to"/"
+        end
+    else
+        redirect_to"/"
+    end
+end
     # Ajoutez ici plus d'actions si nécessaire (par exemple, new, create, edit, update, destroy)
     # si vous voulez permettre aux administrateurs de gérer les produits directement depuis l'application.
   end
